d70cbf4ce6554d28a3d298f8c8bbef358b682b22
JoelLopes949 <joelyalopes2710@gmail.com>
2025-03-26 21:15:16 -0300

    js aula13

---
+++ aula13/ambiente.js
@@ -0,0 +1,25 @@
+//Teste lógico no final
+var c = 1
+do {
+    console.log(`${c}`)
+    c++ 
+} while (c <= 6)
+
+/* Teste lógico no inicio
+var c = 1
+while (c <= 6) {
+    console.log(`${c}`)
+    c++ // c = c + 1
+}
+*/
+
+
+/* Pior maneira de fazer
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+*/
+
