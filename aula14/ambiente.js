622e3f88baeadde764d39d79a992fe75e36b4b11
JoelLopes949 <joelyalopes2710@gmail.com>
2025-03-27 20:29:27 -0300

    update

---
+++ aula14/ambiente.js
@@ -0,0 +1,21 @@
+/*
+var c = 1
+while (c <= 5) {
+    console.log(`Numero: ${c}`)
+    c++
+}
+*/
+
+/*
+var c = 1
+do {
+    console.log(`${c}`)
+    c++
+} while (c <= 5)
+*/
+
+console.log('Vai começar...')
+for (var c = 1; c <=4; c+= 1) {
+    console.log(`Número: ${c}`)
+}
+console.log('FIM.!')
