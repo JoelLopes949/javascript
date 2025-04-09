f74cbd14784722f7c564b45ccd559254dc9e13ab
JoelLopes949 <joelyalopes2710@gmail.com>
2025-04-08 22:00:41 -0300

    .

--- tocador_musica/script.js
+++ tocador_musica/script.js
@@ -5,6 +5,9 @@ const cover = document.getElementById('cover');
 const play = document.getElementById('play');
 const next = document.getElementById('next');
 const previous = document.getElementById('previous');
+const currentProgress = document.getElementById('current-progress');
+const progressContainer = document.getElementById('progress-container');
+
 
 const FearOfTheDark = {
     songName: 'Fear of the Dark (2015 Remaster)',
@@ -56,13 +59,6 @@ function initializeSong() {
     artistName.innerText = playlist[index].artist;   
 }
 
-song.load();
-    if (isPlaying) {
-        song.oncanplaythrough = () => {
-            song.play();
-        };
-    }
-
 function previousSong() {
     if (index === 0) {
         index = playlist.length - 1;
@@ -83,10 +79,24 @@ function nextSong() {
     playSong();
 }
 
+function updateProgressBar() {
+    const barWidth = song.currentTime / song.duration * 100;
+    currentProgress.style.setProperty('--progress', `${barWidth}%`);
+}
+
+function jumpTo(event) {
+    const width = progressContainer.clientWidth;
+    const clickPosition = event.offsetX;
+    const jumpToTime = (clickPosition/width) * song.duration;
+    song.currentTime = jumpToTime;
+}
+
 play.addEventListener('click', playPauseDecider);
 previous.addEventListener('click', previousSong);
 next.addEventListener('click', nextSong);
-song.addEventListener('ended', nextSong);
+song.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', jumpTo);
+
 
 initializeSong();
 
