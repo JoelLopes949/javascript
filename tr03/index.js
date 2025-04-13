f8ba3d5d4076df70f080fade321b5a6f0f33b8f5
JoelLopes949 <joelyalopes2710@gmail.com>
2025-04-13 17:59:19 -0300

    js

---
+++ tr03/index.js
@@ -0,0 +1,10 @@
+function cumprimentarCliente() {
+    console.log('Olá, bom dia, seja bem-vindo ao restaurante Joel');
+    console.log('Hoje temos estrogonofe, feijoada, baião de dois, hamburguer');
+    console.log('Obrigado pela sua visita! Tenha um bom dia e volte sempre!');
+}
+
+cumprimentarCliente();
+cumprimentarCliente();
+cumprimentarCliente();
+cumprimentarCliente();
