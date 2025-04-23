9bfb91fa3b0c69323a83bbc5af34ebc897d2bc4d
JoelLopes949 <joelyalopes2710@gmail.com>
2025-04-23 20:07:05 -0300

    update

---
+++ tr14/index.js
@@ -0,0 +1,28 @@
+const disciplina = 'Python';
+
+switch (disciplina) {
+    case 'Excel': 
+        console.log('Esse é um aluno impressionador de Excel da Hashtag');
+        break;
+    case 'Power Bi':
+        console.log('Esse é um aluno impressionador de Power Bi da Hashtag');
+        break;
+    case 'Python':
+        console.log('Esse é um aluno impressionador de Python da Hashtag');
+        break;
+    case 'Html/CSS':
+        console.log('Esse é um aluno impressionador de Html/CSS da Hashtag');
+        break;
+    case 'VBA': 
+        console.log('Esse é um aluno impressionador de VBA da Hashtag');
+        break;
+    case 'SQL':
+        console.log('Esse é um aluno impressionador de SQL da Hashtag');
+        break;
+        case 'JavaScript':
+        console.log('Esse é um aluno impressionador de JavaScript da Hashtag');
+        break;
+    default:
+        console.log('Esse não é um aluno impressionador da Hashtag')
+}
+
