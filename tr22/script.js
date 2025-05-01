09d12082693af62d5636a14e37ab7ceaf535d9de
JoelLopes949 <joelyalopes2710@gmail.com>
2025-05-01 10:18:52 -0300

    .

---
+++ tr22/script.js
@@ -0,0 +1,9 @@
+function posicaoObtidaComSucesso(geolocalizacao) {
+    console.log(geolocalizacao);
+}
+
+function posicaoNaoPodeSerObtida(erro) {
+    console.error(erro);
+}
+
+navigator.geolocation.getCurrentPosition(posicaoObtidaComSucesso, posicaoNaoPodeSerObtida);
