d539fa55c5cf4c32f790570b2d45b35a6da583fb
JoelLopes949 <joelyalopes2710@gmail.com>
2025-05-05 22:21:53 -0300

    update

--- node_aula/app.js
+++ node_aula/app.js
@@ -4,7 +4,17 @@ import express from  'express';
 const app = express();
 
 app.get('/', (requisicao, resposta) => {
-    resposta.send(tabela2024);
-})
+    resposta.status(200).send(tabela2024);
+});
+
+app.get('/:sigla', (requisicao, resposta) => {
+    const siglaInformada = requisicao.params.sigla.toUpperCase();
+    const time = tabela2024.find(infoTime => infoTime.sigla === siglaInformada);
+    if (!time /**time === undefined */) { // undefined -> Se comporta como falso toda vez que exigimos comportamento de boolean. -> se a variável time for undefined, então ela vai se comporta como (false)  e consequentemente, !time vai se comportar como verdadeiro.
+        resposta.status(404).send('Não existe na série A do Brasileirão um time com a sigla informada.!')
+        return;
+    }
+    resposta.status(200).send(time);
+});
 
 app.listen(300, () => console.log('servidor rodando com sucesso'));
