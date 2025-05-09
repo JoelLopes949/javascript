f61412241b39802497f860666df8a8da3347c9fd
JoelLopes949 <joelyalopes2710@gmail.com>
2025-05-08 22:02:33 -0300

    db

--- node_aula/app.js
+++ node_aula/app.js
@@ -20,14 +20,16 @@ app.get('/:sigla', (requisicao, resposta) => {
 
 app.put('/:sigla', (req, res) => {
     const siglaInformada = req.params.sigla.toUpperCase();
-    const timeSelecionado = tabela2024.find((t) => t.sigla === siglaInformada)
-    tabela2024.find((t) => t.sigla === siglaInformada); // Find operation retained if needed for side effects
+    const timeSelecionado = tabela2024.find((t) => t.sigla === siglaInformada);
+    if (!timeSelecionado) {
+        res.status(404).send('Não existe na série A do Brasileirão um time com a sigla informada')
+    }
     const campos = Object.keys(req.body)
     for (let campo of campos) {
       timeSelecionado[campo] =  req.body[campo]
     }
     res.status(200).send(timeSelecionado);
-})
+});
 
 app.post('/', (req, res) => {
     const novoTime = (req.body);
@@ -38,8 +40,9 @@ app.post('/', (req, res) => {
 app.delete('/:sigla', (req, res) => {
     const siglaInformada = req.params.sigla.toUpperCase();
     const indiceTimeSelecionado = tabela2024.findIndex((t) => t.sigla === siglaInformada);
+    console.log(indiceTimeSelecionado);
     const timeRemovido = tabela2024.splice(indiceTimeSelecionado, 1);
     response.status(200).send(timeRemovido);
-})
+});
 
 app.listen(300, () => console.log('servidor rodando com sucesso'));
