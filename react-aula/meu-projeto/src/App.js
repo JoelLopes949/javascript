354ac84eafbbb6a7d9d757b6f719a5fe223f9a2f
JoelLopes949 <joelyalopes2710@gmail.com>
2025-05-12 20:16:32 -0300

    update

--- react-aula/meu-projeto/src/App.js
+++ react-aula/meu-projeto/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 
 function App() {
+  const name = "Joel"
+  const newName = name.toLocaleUpperCase()
+
+  function sum(a, b) {
+    return a + b
+  }
+
+  const url = "https://picsum.photos/200"
+
   return (
     <div className="App">
-      <h1>Olá React</h1>
-      <p>Meu primeiro projeto em React</p>
+      <h1>Olá, {name}</h1>
+      <p>Seu nome em maiúsculo é {newName}</p>
+      <p>Soma: {sum(1, 2)}</p>
+      <img src={url} alt="Minha imagem" />
     </div>
   );
 }
