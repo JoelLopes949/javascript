c4f96c00838e858f154fd15b36f42151acbafa76
JoelLopes949 <joelyalopes2710@gmail.com>
2025-05-18 18:13:47 -0300

    STATE

--- react-aula/meu-projeto/src/components/Form.js
+++ react-aula/meu-projeto/src/components/Form.js
@@ -1,18 +1,39 @@
+import { useState } from "react"
+
 function Form() {
 
     function cadastrarUsuario(e) {
         e.preventDefault(); // Evita o comportamento padrão do formulário
-        console.log("Cadastrou o usuário"); // Aqui você pode adicionar a lógica para cadastrar o usuário
+        console.log(`Usuário ${name} cadastrado com a senha ${password}`);
     }
+
+    const [name, setName] = useState();
+    const [password, setPassword] = useState();
+
     return (
         <div>
             <h1>Meu cadastro</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite seu nome" />
+                    <label htmlFor="name">Nome: </label>
+                    <input type="text"
+                    id="name" 
+                    name="name" 
+                    placeholder="Digite seu nome"
+                    onChange={(e) => setName(e.target.value)}
+                    />
                 </div>
                 <div>
-                        <input type="submit" value="Cadastrar" />
+                    <label htmlFor="password">Senha: </label>
+                    <input type="password" 
+                    id="password" 
+                    name="password" 
+                    placeholder="Digite a sua senha" 
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <input type="submit" value="Cadastrar" />
                 </div>
             </form>
         </div>
