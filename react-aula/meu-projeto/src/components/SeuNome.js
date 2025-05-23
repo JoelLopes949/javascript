fff9e89a4ca60d17b0059c953d6ecc4749b20167
JoelLopes949 <joelyalopes2710@gmail.com>
2025-05-23 20:14:02 -0300

    atualizacao

--- react-aula/meu-projeto/src/components/SeuNome.js
+++ react-aula/meu-projeto/src/components/SeuNome.js
@@ -3,7 +3,10 @@ function SeuNome({setNome}) {
     
 
     return (
-        
+        <div>
+            <h1>Seu Nome</h1>
+            <input type="text" placeholder="Digite seu nome" onChange={(e) => setNome(e.target.value)} />
+        </div>
     )
 }
 
