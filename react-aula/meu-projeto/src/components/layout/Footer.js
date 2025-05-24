7677b81aac3a65beda932198d5c5afec1f077744
JoelLopes949 <joelyalopes2710@gmail.com>
2025-05-24 18:42:45 -0300

    icons em react

--- react-aula/meu-projeto/src/components/layout/Footer.js
+++ react-aula/meu-projeto/src/components/layout/Footer.js
@@ -1,5 +1,25 @@
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
+import styles from './Footer.module.css';
+import { FaHandScissors } from "react-icons/fa6";
+
 function Footer() {
-    return <footer>Rodapé</footer>
+    return ( 
+    <footer>
+        <ul className={styles.social_list}>
+            <li>
+                <FaFacebook/>
+            </li>
+            <li>
+                <FaInstagram/>
+            </li>
+            <li>
+                <FaTwitter/>
+            </li>
+        </ul>
+        <p>Nosso rodapé</p>
+        <FaHandScissors />
+    </footer>
+    )
 }
 
 export default Footer;
