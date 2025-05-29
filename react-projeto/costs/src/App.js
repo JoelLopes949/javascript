ca808a5de201ff254855b2c5824c67d7b8fef08e
JoelLopes949 <joelyalopes2710@gmail.com>
2025-05-28 21:07:24 -0300

    .

--- react-projeto/costs/src/App.js
+++ react-projeto/costs/src/App.js
@@ -1,21 +1,17 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route , Link} from 'react-router-dom'
 import Home from './components/pages/Home'
 import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
 
 import Container from './components/layout/Container'
-
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-      </div>
+      <Navbar />
       <Container customClass="min-height">
         <Routes>
           <Route path="/" element={<Home />} />
@@ -24,9 +20,9 @@ function App() {
           <Route path="/newproject" element={<NewProject />} />
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer />
     </Router>
-  );
+  )
 }
 
 export default App;
