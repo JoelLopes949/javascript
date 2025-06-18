7c9edf0a73d508e6cc9d554732c7bee1f369c906
JoelLopes949 <joelyalopes2710@gmail.com>
2025-06-17 21:02:55 -0300

    .

---
+++ fundamentos/4_loops.js
@@ -0,0 +1,13 @@
+// for -> mais comum
+
+// estrutura de repetição = loop
+// executan N vezes um código
+
+// for(INCREMENTO, CONDIÇÃO, INCR.) {}
+
+// Incremento = i, j, k, m
+// Condição = i < 10
+// Incrementador = i++
+
+// loop que itera de 0 até
+for(let i = 0)
