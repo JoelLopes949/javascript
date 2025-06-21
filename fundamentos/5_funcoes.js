01f825b1d7f6d85bebdac2e00e95ee0555c31258
JoelLopes949 <joelyalopes2710@gmail.com>
2025-06-21 20:28:08 -0300

    funcoes

--- fundamentos/5_funcoes.js
+++ fundamentos/5_funcoes.js
@@ -43,3 +43,75 @@ console.log(somaDois);
 // A função não precisa de parámetro para ser criado, não precisa de retorno (são opcionais)
 
 //FUNÇÃO ANÔNIMA
+const saudacao = function(nome) {
+    console.log("Olá " + nome)
+}
+
+saudacao("Joel")
+
+
+//Função anônima 2: callback
+// É executar uma função como argumento
+function operacao(a, b, callback) {
+    return(a, b);
+}
+
+let resultadoOperacao = operacao(10, 8, soma)
+
+console.log(resultadoOperacao);
+
+let resultadoDois = operacao(5, 5, function (x, y) {
+    return x - y;
+})
+
+console.log(resultadoDois)
+
+// Arrow functions ou Funções de flecha
+// com uma sintaxe menor
+
+// (arg1, arg2) => {}
+const dividir  = (a, b) => {
+    return a / b;
+}
+
+console.log(dividir(10, 5));
+
+const multiplicacaoArrow = (a, b) => a * b;
+
+// quando ela só tem uma linha, não precisa de: 
+// {}
+// return
+
+console.log(multiplicacaoArrow(10, 10))
+
+const msg = () => console.log("Testando")
+msg();
+
+
+// função que detecta o tipo de dado
+function detectarTipo(valor) {
+    if(typeof valor === "string") {
+        return "String"
+    } else if(typeof valor === "number") {
+        return "Number"
+    } else if(typeof valor === "boolean") {
+        return "Boolean"
+    } else {
+        return "Tipo desconhecido"
+    }
+}
+
+console.log(detectarTipo("test"))
+console.log(detectarTipo(true))
+console.log(detectarTipo(2))
+
+
+// Condicional ternária 
+
+// condição/expressão ? TRUE : FALSE
+
+const idade = 14;
+
+const ehMaioDeIdade = idade >= 18 ? "É maior" : "Não é maior"
+
+console.log(ehMaioDeIdade);
