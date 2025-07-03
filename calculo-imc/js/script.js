33e70e4fb3e0dc6ecd0bdc23273732a5e473f5a3
JoelLopes949 <joelyalopes2710@gmail.com>
2025-07-02 21:03:35 -0300

    imc js

--- calculo-imc/js/script.js
+++ calculo-imc/js/script.js
@@ -79,6 +79,8 @@ function createTable(data) {
 function cleanInputs() {
     heightInput.value = "";
     weightInput.value = "";
+    imcNumber.classList = "";
+    imcInfo.classList = "";
 }
 
 function validDigits(text) {
@@ -134,11 +136,40 @@ calcBtn.addEventListener("click", (e) => {
     imcNumber.innerText = imc;
     imcInfo.innerText = info;
 
+    switch(info) {
+
+        case "Magreza":
+            imcNumber.classList.add("low");
+            imcInfo.classList.add("low");
+            break;
+        case "Normal":
+            imcNumber.classList.add("good");
+            imcInfo.classList.add("good");
+            break;
+        case "Sobrepeso":
+            imcNumber.classList.add("medium");
+            imcInfo.classList.add("medium");
+            break;
+        case "Obesidade":
+            imcNumber.classList.add("low");
+            imcInfo.classList.add("low");
+            break;
+        case "Obesidade grave":
+            imcNumber.classList.add("high");
+            imcInfo.classList.add("high");
+            break;
+    }
+
     showOrHideResults();
-})
+});
 
 clearBtn.addEventListener("click", (e) => {
     e.preventDefault();
 
     cleanInputs();
-})
+});
+
+backBtn.addEventListener("click", () => {
+    cleanInputs();
+    showOrHideResults();
+});
