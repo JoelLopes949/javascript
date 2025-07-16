f8fb615461cca33d58158b1b139660c8c986644c
JoelLopes949 <joelyalopes2710@gmail.com>
2025-07-15 21:38:31 -0300

    final-test

--- xlanding/src/javascript/script.js
+++ xlanding/src/javascript/script.js
@@ -9,13 +9,53 @@ $(document).ready(function() {
 
     $(window).on('scroll', function() {
         const header = $('header');
-        
         const scrollPosition = $(window).scrollTop() - header.outerHeight();
-
+        
+        let activeSecctionIndex = 0;
+        
         if (scrollPosition <= 0) {
             header.css('box-shadow', 'none');
         } else {
             header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)');
         }
+
+        sections.each(function(i) {
+            const section = $(this);
+            const sectionTop = section.offset().top - 96;
+            const sectionBottom = sectionTop + section.outerHeight();
+
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                activeSecctionIndex = i;
+                return false; // Break the loop
+            }
+        })
+
+        navItem.removeClass('active');  
+        $(navItem[activeSecctionIndex]).addClass('active');
     });
+
+    ScrollReveal().reveal('#cta', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('.dish', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('#testimonial_chef', {
+        origin: 'left',
+        duration: 1000,
+        distance: '20%'
+    })
+
+    ScrollReveal().reveal('.feedback', {
+        origin: 'right',
+        duration: 1000,
+        distance: '20%'
+    })
+
 });
