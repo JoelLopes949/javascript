c1656ebc5a0014129fe45e2a2ada7aa28e7e60fe
JoelLopes949 <joelyalopes2710@gmail.com>
2026-03-03 21:04:13 -0300

    crud

--- apirestnodejs/app.js
+++ apirestnodejs/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mysql from 'mysql2';
 
 var app = express();
+app.use(express.json()); // para poder interpretar JSON en el cuerpo de las solicitudes
 
 // establecemos los parámetros de conexión
 var conexion = mysql.createConnection({
@@ -23,6 +24,68 @@ conexion.connect(error => {
 // ruta de prueba
 app.get('/', (req, res) => {
     res.send('Ruta INICIO');
+});
+
+// mostrar todos los artículos
+app.get('/api/article', (req, res) => {
+    conexion.query('SELECT * FROM article', (error, filas) => {
+        if (error) {
+            throw error;
+        } else {
+            res.send(filas);
+        }
+    });
+});
+
+// mostrar un artículo por su id
+app.get('/api/article/:id', (req, res) => {
+    conexion.query('SELECT * FROM article WHERE id = ?', [req.params.id], (error, fila) => {
+        if (error) {
+            throw error;
+        } else {
+            //res.send(fila);
+            res.send(fila[0].descripcion); // enviamos solo el primer elemento del array
+        }
+    });
+});
+
+app.post('/api/article', (req, res) => {
+    let data = { descripcion: req.body.descripcion, precio: req.body.precio, stock: req.body.stock };
+    let sql = 'INSERT INTO article SET ?';
+    conexion.query(sql, data, function(error, results) {
+        if (error) {
+            throw error;
+        } else {
+            res.send(results);
+        }
+    });
+});
+
+// editar artículo
+app.put('/api/article/:id', (req, res) => {
+    let id = req.params.id;
+    let descripcion = req.body.descripcion;
+    let precio = req.body.precio;
+    let stock = req.body.stock;
+    let sql = 'UPDATE article SET descripcion = ?, precio = ?, stock = ? WHERE id = ?';
+    conexion.query(sql, [descripcion, precio, stock, id], function(error, results) {
+        if (error) {
+            throw error;
+        } else {
+            res.send(results);
+        }
+    });
+});
+    
+// eliminar artículo
+app.delete('/api/article/:id', (req, res) => {
+    conexion.query('DELETE FROM article WHERE id = ?', [req.params.id], function(error, filas) {
+        if (error) {
+            throw error;
+        } else {
+            res.send(filas);
+        }
+    });
 });
 
 const puerto = process.env.PUERTO || 3000;
